66eabe417e31193f3b6b1d0bdef72aa5bf4f8d2b
Matias Halles <matias.halles@gmail.com>
2020-08-03 12:01:28 -0400

    Fix paths for webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,16 +3,16 @@ const mix = require('laravel-mix');
 mix.sourceMaps();
 
 mix
-  .sass('themes/silverstripe-template-theme/src/scss/main.scss', 'themes/silverstripe-template-theme/dist/css/')
+  .sass('app/themes/silverstripe-template-theme/src/scss/main.scss', 'app/themes/silverstripe-template-theme/dist/css/')
   .options({
     processCssUrls: false,
   });
 
-mix.js('themes/silverstripe-template-theme/src/js/main.js', 'themes/silverstripe-template-theme/dist/js/');
+mix.js('app/themes/silverstripe-template-theme/src/js/main.js', 'app/themes/silverstripe-template-theme/dist/js/');
 
-mix.copyDirectory('themes/silverstripe-template-theme/src/ico', 'themes/silverstripe-template-theme/dist/ico/');
-mix.copyDirectory('themes/silverstripe-template-theme/src/img', 'themes/silverstripe-template-theme/dist/img/');
-mix.copyDirectory('themes/silverstripe-template-theme/src/fonts', 'themes/silverstripe-template-theme/dist/fonts/');
+mix.copyDirectory('app/themes/silverstripe-template-theme/src/ico', 'app/themes/silverstripe-template-theme/dist/ico/');
+mix.copyDirectory('app/themes/silverstripe-template-theme/src/img', 'app/themes/silverstripe-template-theme/dist/img/');
+mix.copyDirectory('app/themes/silverstripe-template-theme/src/fonts', 'app/themes/silverstripe-template-theme/dist/fonts/');
 
 mix
   .sass('modules/documents-library/scss/documents-library.scss', 'modules/documents-library/dist/css/')
