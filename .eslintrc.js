5e1fca7245be1c3a7cfb0d1bf3c0021b20fa63cd
Matias Halles <matias.halles@gmail.com>
2021-02-24 15:48:07 +1300

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,21 @@
 /** @type {import('eslint').Linter.Config} */
 const config = {
-  extends: ['ackama']
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    commonjs: true,
+    node: true
+  },
+  extends: ['ackama'],
+  ignorePatterns: [
+    'node_modules/',
+    'vendor/',
+    'themes/watea/',
+    'themes/starter/',
+    'app/themes/silverstripe-template-theme/dist/'
+  ]
 };
 
 module.exports = config;
